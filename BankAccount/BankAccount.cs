66322d11cefb85c682a4a476a576a660dfa38de3
desjardinsm-etsu <desjardinsm@etsu.edu>
2022-11-16 11:56:16 -0500

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        private int accountNumber;
+        private char type;
+        private decimal balance;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public BankAccount(char type, decimal intialDeposit, int accountNumber, string firstName, string lastName)
+        {
+            this.accountNumber = accountNumber;
+            this.type = type;
+            Deposit(intialDeposit);
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        //This is the Copy Constructor!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        public BankAccount(BankAccount otherObj) 
+        { 
+            this.accountNumber = otherObj.accountNumber;
+            this.type = otherObj.type;
+            this.balance= otherObj.balance;
+            this.FirstName= otherObj.FirstName;
+            this.LastName= otherObj.LastName;
+        }
+
+        public bool Deposit(decimal amount)
+        {
+            if (amount < 0)
+                return false;
+            else
+            {
+                balance += amount;
+                return true;
+            }
+        }
+
+        public decimal GetBalance()
+        {
+            return this.balance;
+        }
+
+        public string GetBalanceAsDollars()
+        {
+            return this.balance.ToString("C");
+        }
+
+        public override string ToString()
+        {
+            string msg = "";
+
+            msg += this.accountNumber + "\n";
+            msg += this.type + "\n";
+            msg += this.GetBalanceAsDollars() + "\n";
+            msg += FirstName + "\n";
+           msg += LastName + "\n";
+
+            return msg;
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankAccount = new BankAccount('h', 12.23M, 100, "Kay", "Kay");
+
+            Console.WriteLine(bankAccount.FirstName);
+            Console.WriteLine(bankAccount.LastName);
+
+            bankAccount.FirstName = "New Kay";
+            bankAccount.LastName = "New Kay";
+
+            Console.WriteLine(bankAccount.FirstName);
+            Console.WriteLine(bankAccount.LastName);
+
+            Console.WriteLine(bankAccount.Deposit(12.23M));
+
+            Console.WriteLine(bankAccount.GetBalanceAsDollars());
+
+
+            Console.Write("How much money do you want to deposit? ");
+            decimal userInput = Convert.ToDecimal(Console.ReadLine());
+
+            while(!bankAccount.Deposit(userInput))
+            {
+                Console.WriteLine("Please do not enter a negative number!");
+                Console.Write("How much money do you want to deposit? ");
+                userInput = Convert.ToDecimal(Console.ReadLine());
+            }
+
+            Console.WriteLine(bankAccount.GetBalanceAsDollars());
+
+            Console.WriteLine(bankAccount);
+
+            //this is a shallow copy
+            //var bankaccount2 = bankAccount;
+
+            var bankaccount2 = new BankAccount(bankAccount);
+
+            Console.WriteLine(bankaccount2);
+
+
+            bankaccount2.Deposit(345);
+            Console.WriteLine(bankAccount);
+            Console.WriteLine(bankaccount2);
+
+
+        }
+
+    }
+}
